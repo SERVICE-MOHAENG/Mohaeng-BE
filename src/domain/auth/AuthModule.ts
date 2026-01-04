66a0ce6b96e0636d6e752976985ffe8f08dfc618
하동건 <163298112+dongguli08@users.noreply.github.com>
2026-01-04 14:18:03 +0900

    feat (#26) : OAuth 코드 저장소 추가

--- src/domain/auth/AuthModule.ts
+++ src/domain/auth/AuthModule.ts
@@ -4,6 +4,7 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '../../global/jwt/Jwt.module';
 import { RefreshToken } from './entity/RefreshToken.entity';
 import { RefreshTokenRepository } from './persistence/RefreshTokenRepository';
+import { OAuthCodeRepository } from './persistence/OAuthCodeRepository';
 import { UserModule } from '../user/UserModule';
 import { AuthController } from './presentation/AuthController';
 import { AuthService } from './service/AuthService';
@@ -13,7 +14,7 @@ import { GoogleStrategy } from './strategy/google.strategy';
  * AuthModule
  * @description
  * - 인증 도메인 모듈
- * - 로그인, 토큰 관리
+ * - 로그인, 토큰 관리, OAuth 인증 코드 관리
  */
 @Module({
   imports: [
@@ -23,7 +24,12 @@ import { GoogleStrategy } from './strategy/google.strategy';
     UserModule, // UserService를 사용하기 위해 import
   ],
   controllers: [AuthController],
-  providers: [AuthService, RefreshTokenRepository, GoogleStrategy],
+  providers: [
+    AuthService,
+    RefreshTokenRepository,
+    OAuthCodeRepository,
+    GoogleStrategy,
+  ],
   exports: [AuthService],
 })
 export class AuthModule {}
