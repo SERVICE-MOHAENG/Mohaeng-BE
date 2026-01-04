5c549b72171b04c3d062919f205efc72615e5ce4
하동건 <163298112+dongguli08@users.noreply.github.com>
2026-01-04 23:10:30 +0900

    refactor (#develop) : cors 수정

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,8 @@ async function bootstrap() {
 
   app.use(cookieParser());
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-    credentials: true,
+    origin: '*',
+    credentials: false,
   });
 
   app.setGlobalPrefix('api');
