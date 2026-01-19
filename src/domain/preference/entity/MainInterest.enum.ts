a28b22a448aac353ace2d7492c425788dae22a7b
하동건 <163298112+dongguli08@users.noreply.github.com>
2026-01-19 17:27:42 +0900

    feat (#54) : 핵심 관심사 enum 추가

---
+++ src/domain/preference/entity/MainInterest.enum.ts
@@ -0,0 +1,13 @@
+/**
+ * MainInterest Enum
+ * @description
+ * - 핵심 관심사 열거형
+ * - 설문 6번: 핵심 관심사
+ * - 사용자가 여행지에서 반드시 하고 싶은 활동
+ */
+export enum MainInterest {
+  PHOTO_SPOTS = 'PHOTO_SPOTS', // 사진/감성
+  SHOPPING = 'SHOPPING', // 쇼핑
+  ACTIVITY = 'ACTIVITY', // 액티비티
+  ART_CULTURE = 'ART_CULTURE', // 예술/관람
+}
