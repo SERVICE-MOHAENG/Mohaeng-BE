0df782903a1a50dcf2f20892661f85d1ebecb8de
하동건 <163298112+dongguli08@users.noreply.github.com>
2026-01-19 17:27:43 +0900

    feat (#54) : 여행 스타일 enum 추가

---
+++ src/domain/preference/entity/TravelStyle.enum.ts
@@ -0,0 +1,18 @@
+/**
+ * TravelStyle Enum
+ * @description
+ * - 여행 스타일 열거형
+ * - 사용자 선호도 및 지역 특성 분류에 사용
+ */
+export enum TravelStyle {
+  NATURE_HEALING = 'NATURE_HEALING', // 자연/힐링
+  CITY_SHOPPING = 'CITY_SHOPPING', // 도시/쇼핑
+  HISTORY_CULTURE = 'HISTORY_CULTURE', // 역사/문화
+  ACTIVITY_ADVENTURE = 'ACTIVITY_ADVENTURE', // 액티비티/모험
+  FOOD_GOURMET = 'FOOD_GOURMET', // 음식/미식
+  BEACH_RESORT = 'BEACH_RESORT', // 해변/리조트
+  MOUNTAIN_HIKING = 'MOUNTAIN_HIKING', // 산/등산
+  THEME_PARK = 'THEME_PARK', // 테마파크
+  NIGHT_VIEW = 'NIGHT_VIEW', // 야경
+  PHOTOGRAPHY = 'PHOTOGRAPHY', // 사진촬영
+}
