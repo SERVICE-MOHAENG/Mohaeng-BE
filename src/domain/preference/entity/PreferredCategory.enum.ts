bc5a00869750fced11cdd7160e79759e2985fc40
하동건 <163298112+dongguli08@users.noreply.github.com>
2026-01-19 17:27:42 +0900

    feat (#54) : 선호 카테고리 enum 추가

---
+++ src/domain/preference/entity/PreferredCategory.enum.ts
@@ -0,0 +1,20 @@
+/**
+ * PreferredCategory Enum
+ * @description
+ * - 관심 카테고리 열거형
+ * - 지역의 주요 카테고리 분류에 사용
+ */
+export enum PreferredCategory {
+  BEACH = 'BEACH', // 해변
+  MOUNTAIN = 'MOUNTAIN', // 산
+  MUSEUM = 'MUSEUM', // 박물관
+  TEMPLE = 'TEMPLE', // 사찰/성당
+  PALACE = 'PALACE', // 궁전
+  PARK = 'PARK', // 공원
+  RESTAURANT = 'RESTAURANT', // 레스토랑
+  CAFE = 'CAFE', // 카페
+  MARKET = 'MARKET', // 시장
+  SHOPPING_MALL = 'SHOPPING_MALL', // 쇼핑몰
+  NIGHTLIFE = 'NIGHTLIFE', // 나이트라이프
+  LANDMARK = 'LANDMARK', // 랜드마크
+}
