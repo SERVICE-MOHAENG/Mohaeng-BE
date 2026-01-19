c95ae8c2d1ddd2ce1df3b707cfcd7e799fd72039
하동건 <163298112+dongguli08@users.noreply.github.com>
2026-01-19 17:27:42 +0900

    feat (#54) : 식도락 성향 enum 추가

---
+++ src/domain/preference/entity/FoodPersonality.enum.ts
@@ -0,0 +1,11 @@
+/**
+ * FoodPersonality Enum
+ * @description
+ * - 식도락 성향 열거형
+ * - 설문 5번: 식도락 성향
+ */
+export enum FoodPersonality {
+  LOCAL_ADVENTURE = 'LOCAL_ADVENTURE', // 로컬 도전
+  SAFE_CHOICE = 'SAFE_CHOICE', // 안전 지향
+  CAFE_DESSERT = 'CAFE_DESSERT', // 카페인
+}
