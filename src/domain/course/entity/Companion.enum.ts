86bec9c81385d3219153eb4f52f4c806620811e4
하동건 <163298112+dongguli08@users.noreply.github.com>
2026-02-04 08:13:07 +0900

    feat (#69) : 로드맵 설문 ENUM 추가

---
+++ src/domain/course/entity/Companion.enum.ts
@@ -0,0 +1,18 @@
+/**
+ * Companion Enum
+ * @description
+ * - 동행자 유형 열거형
+ * - 로드맵 생성 설문: 동행자
+ */
+export enum Companion {
+  FAMILY = 'FAMILY', // 가족
+  FRIENDS = 'FRIENDS', // 친구
+  COUPLE = 'COUPLE', // 연인
+  SPOUSE = 'SPOUSE', // 배우자
+  CHILDREN = 'CHILDREN', // 아이
+  PARENTS = 'PARENTS', // 부모님
+  TEACHER = 'TEACHER', // 선생님
+  STUDENTS = 'STUDENTS', // 학생
+  COLLEAGUES = 'COLLEAGUES', // 직장 동료
+  SOLO = 'SOLO', // 혼자
+}
