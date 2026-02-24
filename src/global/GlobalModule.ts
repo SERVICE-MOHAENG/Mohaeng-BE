c6aea6ec3dfffb567543a93927d284afa6e6834f
하동건 <163298112+dongguli08@users.noreply.github.com>
2026-02-24 10:47:47 +0900

    feat (#133) : AWS S3 글로벌 모듈 및 서비스 추가

--- src/global/GlobalModule.ts
+++ src/global/GlobalModule.ts
@@ -2,14 +2,15 @@ import { Global, Module } from '@nestjs/common';
 import { UserModule } from '../domain/user/UserModule';
 import { JwtModule } from './jwt/Jwt.module';
 import { RedisModule } from './redis/Redis.module';
+import { S3Module } from './s3/S3Module';
 import { AdminAuthGuard } from './guards/AdminAuth.guard';
 import { UserAuthGuard } from './guards/UserAuth.guard';
 
 @Global()
 @Module({
-  imports: [JwtModule, RedisModule, UserModule],
+  imports: [JwtModule, RedisModule, UserModule, S3Module],
   controllers: [],
   providers: [AdminAuthGuard, UserAuthGuard],
-  exports: [AdminAuthGuard, UserAuthGuard, JwtModule, RedisModule],
+  exports: [AdminAuthGuard, UserAuthGuard, JwtModule, RedisModule, S3Module],
 })
 export class GlobalModule {}
